a3f6689b36201c1004274e4b240bfe62547b6f8b
Muskan6wq <muskanbhushan2@gmail.com>
2026-05-16 14:44:45 +0530

    Added Checkov IaC security scanning

---
+++ terraform/main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_security_group" "flask_sg" {
+
+  name = "flask-security-group"
+
+  ingress {
+    from_port   = 5001
+    to_port     = 5001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "devsecops_server" {
+
+  ami           = "ami-0f58b397bc5c1f2e8"
+
+  instance_type = "t2.micro"
+
+  security_groups = [aws_security_group.flask_sg.name]
+
+  tags = {
+    Name = "DevSecOps-Server"
+  }
+}
+
+
