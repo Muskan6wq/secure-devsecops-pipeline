448fa5823720060752119151c937def3c7cfa0c1
Muskan6wq <muskanbhushan2@gmail.com>
2026-05-17 16:41:23 +0530

    Updated staging environment

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,6 +20,19 @@ resource "aws_security_group" "flask_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+   from_port   = 5002
+   to_port     = 5002
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+
+ingress {
+  from_port   = 5003
+  to_port     = 5003
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
   egress {
     from_port   = 0
     to_port     = 0
